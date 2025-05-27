2c33951912a470dcd17dddcba70b621cee0d243d
Cornelius Kibet <kibetcornelius56@gmail.com>
2025-05-27 11:27:36 +0300

    Fixing equation numbering issue

--- source/_ext/equation_reset.py
+++ source/_ext/equation_reset.py
@@ -1,5 +1,7 @@
-def setup(app):
-    app.connect('html-page-context', add_reset_js)
+# extensions/equation_reset.py
+
+from sphinx.util import logging
+logger = logging.getLogger(__name__)
 
 def add_reset_js(app, pagename, templatename, context, doctree):
     chapter_number = extract_chapter_number(pagename)
@@ -8,25 +10,25 @@ def add_reset_js(app, pagename, templatename, context, doctree):
 
     reset_script = f"""
     <script>
-      MathJax.texReset();
-      MathJax.tags.reset();
-      MathJax.tags.counter = 0;
-      MathJax.tags.allLabels = {{}};
-      MathJax.config.tex.tagFormat = {{
+    MathJax.texReset();
+    MathJax.tags.reset();
+    MathJax.tags.counter = 0;
+    MathJax.tags.allLabels = {{}};
+    MathJax.config.tex.tagFormat = {{
         number: function(n) {{ return "{chapter_number}." + n }}
-      }};
-      MathJax.startup.getComponents();
+    }};
+    MathJax.startup.getComponents();
     </script>
     """
 
     context['metatags'] += reset_script
 
 def extract_chapter_number(pagename):
-    # You can improve this logic based on your actual file structure
-    if "chapter2" in pagename.lower():
+    if "chapter2" in pagename:
         return "2"
-    elif "chapter3" in pagename.lower():
+    elif "chapter3" in pagename:
         return "3"
-    elif "chapter4" in pagename.lower():
-        return "4"
     return None
+
+def setup(app):
+    app.connect('html-page-context', add_reset_js)
